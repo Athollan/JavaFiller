289181c912eb976826641db46044ef8ad19ba3b7
Andrew Hollands <athollan@edu.uwaterloo.ca>
2019-05-13 14:08:22 -0400

    second commit

--- NetBeansProjects/Java 2/FillerJava/src/fillerjava/FillerJava.java
+++ NetBeansProjects/Java 2/FillerJava/src/fillerjava/FillerJava.java
@@ -10,12 +10,12 @@ package fillerjava;
  * @author hollands251
  */
 public class FillerJava {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("tr");
     }
     
-}
+} 
