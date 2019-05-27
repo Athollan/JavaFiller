1932a9cba58be78133f3bed486ffc51f857447a5
Andrew Hollands <athollan@edu.uwaterloo.ca>
2019-05-27 13:04:59 -0400

    git with branch

--- NetBeansProjects/Java 2/FillerJava/src/fillerjava/FillerJava.java
+++ NetBeansProjects/Java 2/FillerJava/src/fillerjava/FillerJava.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//there ///chchchchchanges
 package fillerjava;
 
 public class FillerJava {
+    public static int a = 5;
     public static void main(String[] args) {
         System.out.println("tr");
     } 
